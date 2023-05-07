2c60d6269f6c813ceb0d21014805d79349cd476d
h083onis <108774049+h083onis@users.noreply.github.com>
2023-05-08 01:12:19 +0900

    axiosパッケージの追加

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import logo from "./logo.svg";
+import axios from "axios";
 
 function App() {
+  const [text, setText] = useState("");
+
+  const handleClick = () => {
+    axios.get("http://127.0.0.1:8000/api/test").then((res) => {
+      console.log(res);
+      setText(res.data);
+    });
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button type="button" onClick={handleClick}>
+          PUSH!!
+        </button>
+        <h1>{text}</h1>
       </header>
     </div>
   );
